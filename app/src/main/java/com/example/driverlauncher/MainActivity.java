a01de274f17df7110fd36a62d13055fc4c822125
Kayle Brown <KLBrown7@student.fullsail.edu>
2023-06-25 22:36:37 -0400

    added a camera permissions function will work on display later

--- app/src/main/java/com/example/driverlauncher/MainActivity.java
+++ app/src/main/java/com/example/driverlauncher/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.driverlauncher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.GridLayout;
+import android.widget.Button;
 import android.widget.GridView;
-import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
@@ -19,6 +25,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_CAMERA_PERMISSION = 2;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     List<AppObject> installedAppList = new ArrayList<>();
-    private void initializeDrawer(){
+
+    private void initializeDrawer() {
 
         View mBottomSheet = findViewById(R.id.bottomSheet);
         final GridView mDrawerGridView = findViewById(R.id.drawerGrid);
@@ -40,24 +51,65 @@ public class MainActivity extends AppCompatActivity {
         mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), installedAppList));
     }
 
-    private List<AppObject> getInstalledAppList(){
+    private List<AppObject> getInstalledAppList() {
         List<AppObject> list = new ArrayList<>();
 
         Intent intent = new Intent(Intent.ACTION_MAIN, null);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> untreatedAppList = getApplicationContext().getPackageManager().queryIntentActivities(intent,0);
+        List<ResolveInfo> untreatedAppList = getApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
 
-        for (ResolveInfo untreatedApp : untreatedAppList){
+        for (ResolveInfo untreatedApp : untreatedAppList) {
             String appName = untreatedApp.activityInfo.loadLabel(getPackageManager()).toString();
             String appPackageName = untreatedApp.activityInfo.packageName;
             Drawable appImage = untreatedApp.activityInfo.loadIcon(getPackageManager());
 
             AppObject app = new AppObject(appPackageName, appName, appImage);
-            if (!list.contains(app)){
+            if (!list.contains(app)) {
                 list.add(app);
             }
         }
 
         return list;
     }
+
+    public void capturePhoto(View view) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } else {
+            Toast.makeText(this, "No camera app available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Uri imageUri = data.getData();
+            // handle the captured image, such as displaying it in an ImageView
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
